bbd5a6704018bf20c8cd8d6bcc35493560a230f1
Vinh Tran <vctran62@users.noreply.github.com>
2021-03-26 17:55:34 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-#File =provider.tf
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-#      version = "2.46.0"
-    }
-  }
-}
-
 provider "azurerm" {
   subscription_id = "${var.subscriptionId}"  
   client_id       = "${var.clientId}"
