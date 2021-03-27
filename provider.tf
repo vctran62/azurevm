592e425f14a6938e5b37a0b7c8a45cab44bbd960
Vinh Tran <vctran62@users.noreply.github.com>
2021-03-27 09:56:27 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,10 @@
 provider "azurerm" {
-  subscription_id = "${var.subscriptionId}"  
+  
+  subscription_id = "${var.subscriptionId}"
   client_id       = "${var.clientId}"
   client_secret   = "${var.clientSecret}"
   tenant_id       = "${var.tenantId}"
-  features {}
+
+#  features {}
+   version = "=1.44.0"
 }
