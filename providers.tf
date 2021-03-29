739f95f7616c0eb7ed2d87460172db687eaac097
Vinh Tran <vctran62@users.noreply.github.com>
2021-03-29 10:55:09 -0400

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,10 @@
+#File =provider.tf
+provider "azurerm" {
+  
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "${var.clientId}"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "${var.tenantId}"
+
+  version = "=1.44.0"
+}
