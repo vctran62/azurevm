15a0f4c3bd789baf90f673c4f5210c4c10f9ab49
Vinh Tran <vctran62@users.noreply.github.com>
2021-03-29 20:22:30 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,18 +1,18 @@
 #File =var.tf
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default     = "mytesttf"
+  default     = "vcttf"
 }
 
 variable "region" {
   description = "The Azure Region in which all resources in this example should be created"
-   default     = "uksouth"
+   default     = "eastus"
 }
 
 
 variable "resourceGroup" {
   description = "The Azure Region in which all resources in this example should be created"
-   default     = "myrgtestman"
+   default     = "vinhRG"
 }
 
 
@@ -64,7 +64,7 @@ variable "image_version" {
 }
 variable "hostname" {
   description = "VM name referenced also in storage-related names."
-  default="tf"
+  default="vct1"
 }
 
 
